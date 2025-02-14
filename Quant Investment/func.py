967cef0834a9f12ba9a454a943207c65638359ee
Junghooni <69448218+Junghooni@users.noreply.github.com>
2025-02-14 13:24:20 +0900

    Add files via upload

---
+++ Quant Investment/func.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"""
+x = 4
+def sqrt(x):
+    res = x**(1 /2)
+    return res
+
+print(sqrt(x))
+
+
+a = 5
+y = 7
+def multiply(a,y):
+    res = a**y
+    return res
+
+print(multiply(a,y))
+"""
+
+"""
+import selenium.webdriver
+print(dir(selenium.webdriver)[1:20])
+"""
+
+"""
+import seaborn as sns
+
+iris = sns.load_dataset('iris')
+print(iris)
+"""
+
+"""
+# 모든 함수를 불러오는 것이 아닌 특정 함수만 선택해서 불러올 수 있다.
+from seaborn import load_dataset
+    #패키지 이름      특정 함수
+print(load_dataset('iris'))
+
+"""
+
+"""
+#파이썬 내장 함수 sum([])
+print(sum([1,2]))
+"""
+
+"""
+import pandas as pd
+df = pd.DataFrame({'x' : [1,2,3]})
+print(df)
+"""
